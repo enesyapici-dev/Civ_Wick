4d2a287a81ba7372635a1d151a6ae070dd511f7a
Enes Yapici <enesyapici.dev@gmail.com>
2025-10-27 21:53:49 +0300

    v0.8.1 Fix Collectible Bugs

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -7,15 +7,18 @@ public class PlayerInteractionController : MonoBehaviour
     {
         if (other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
         {
-            other.gameObject.GetComponent<GoldWheatCollectible>().Collect();
+
+            other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
         }
         if (other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
         {
-            other.gameObject.GetComponent<HolyWheatCollectible>().Collect();
+
+            other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
         }
         if (other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
         {
-            other.gameObject.GetComponent<RottenWheatCollectible>().Collect();
+
+            other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
         }
     }
 }
