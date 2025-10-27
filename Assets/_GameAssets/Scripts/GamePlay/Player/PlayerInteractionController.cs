39a2a0b0e718d3c5427162f78d631e75c43a7030
Enes Yapici <enesyapici.dev@gmail.com>
2025-10-27 21:59:29 +0300

    v0.8.2 Fix Collider Bug

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -7,17 +7,14 @@ public class PlayerInteractionController : MonoBehaviour
     {
         if (other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
         {
-
             other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
         }
         if (other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
         {
-
             other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
         }
         if (other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
         {
-
             other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
         }
     }
